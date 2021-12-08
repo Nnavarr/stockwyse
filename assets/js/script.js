0138dfd645a28a2670474babfb67548e000e5c5f
eon <noe.navarro1019@gmail.com>
2021-12-08 00:28:46 -0700

    tested yahoo finance api call, set up test case for Apple pull

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+
+var apiKey = 'OeK23R7rjQ4edxtzHNhH44rjXhs0dZJE1kxtmnfG';
+var yahooChartUrl = 'https://yfapi.net/v8/finance/spark?interval=1d&range=3mo&symbols=AAPL';
+
+// fetch process for yahoo finance api 
+fetch(yahooChartUrl, {
+	"method": "GET",
+	"headers": {
+		"x-api-key": apiKey
+	}
+})
+.then(response => {
+	console.log(response);
+    response.json().then(function(data) {
+        console.log(data);
+    })
+})
+.catch(err => {
+	console.error(err);
+});
+
