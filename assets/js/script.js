8ca2009b3948d05b119ac44ff654480f1b35e785
eon <noe.navarro1019@gmail.com>
2021-12-08 20:03:52 -0700

    testing wallstreetbets api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,36 @@
 
-var apiKey = 'OeK23R7rjQ4edxtzHNhH44rjXhs0dZJE1kxtmnfG';
-var yahooChartUrl = 'https://yfapi.net/v8/finance/spark?interval=1d&range=3mo&symbols=AAPL';
+// var apiKey = 'OeK23R7rjQ4edxtzHNhH44rjXhs0dZJE1kxtmnfG';
+// var yahooChartUrl = 'https://yfapi.net/v8/finance/spark?interval=1d&range=3mo&symbols=AAPL';
 
-// fetch process for yahoo finance api 
-fetch(yahooChartUrl, {
+// // fetch process for yahoo finance api 
+// fetch(yahooChartUrl, {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-api-key": apiKey
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+//     response.json().then(function(data) {
+//         console.log(data);
+//     })
+// })
+
+
+// wallstreetbets API
+var wallstreetBetsUrl = 'https://tradestie.com/api/v1/apps/reddit';
+
+fetch(wallstreetBetsUrl, {
+	'mode': 'no-cors',
 	"method": "GET",
-	"headers": {
-		"x-api-key": apiKey
+	headers: {
+		'Content-Type': 'application/json'
 	}
-})
-.then(response => {
+	}).then(function(response) {
 	console.log(response);
-    response.json().then(function(data) {
-        console.log(data);
-    })
-})
-.catch(err => {
-	console.error(err);
-});
 
+	// response.json().then(function(test) {
+	// 	console.log(test);
+	// })
+
+})
